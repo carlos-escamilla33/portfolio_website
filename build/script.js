7f9746826854e255b6c5f4553f6a619d890c303a
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2024-08-13 18:09:04 -0700

    fix mobile view black bar bug

--- build/script.js
+++ build/script.js
@@ -1,11 +1,16 @@
 const mobileHamburgerMenu = document.querySelector(".mobile-menu");
+const navLinks = document.querySelector(".nav-links");
 
 function onToggleMenu(e) {
     const img = e.target;
     if (img.src.includes("menu")) {
         img.src = "./img/close.png";
+        navLinks.classList.remove("top-[-100%]")
+        navLinks.classList.add("top-[9%]")
     } else {
         img.src = "./img/menu.png";
+        navLinks.classList.remove("top-[9%]")
+        navLinks.classList.add("top-[-100%]")
     }
 }
 
